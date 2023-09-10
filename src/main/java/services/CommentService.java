f4f3b24b7fbd10c9c1ac42751bd8d3a97fde2b4e
osparking <jbpark03@gmail.com>
2023-09-10 23:02:24 +0900

    makes bean, uses logger instead of println

--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -1,5 +1,16 @@
 package services;
 
-public class CommentService {
+import java.util.logging.Logger;
 
+import org.springframework.stereotype.Service;
+
+import domain.Comment;
+
+@Service
+public class CommentService {
+	private Logger logger =
+			Logger.getLogger(CommentService.class.getName());
+	public void publishComment(Comment comment) {
+		logger.info("출판한 코멘트: " + comment.getText());
+	}
 }
