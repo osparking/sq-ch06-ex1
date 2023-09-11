71957bf3ebf0292efb44ac69a5d54e6f5d974e97
osparking <jbpark03@gmail.com>
2023-09-11 23:05:31 +0900

    use my utility for utf-8 code logging

--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -1,31 +1,16 @@
 package services;
 
-import java.io.UnsupportedEncodingException;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.springframework.stereotype.Service;
 
 import domain.Comment;
+import util.MyUtil;
 
 @Service
 public class CommentService {
-	private Logger logger =
-			Logger.getLogger(CommentService.class.getName());
-	{
-		var handler = new ConsoleHandler();
-		try {
-			handler.setEncoding("UTF-8");
-			logger.addHandler(handler);
-		} catch (SecurityException e1) {
-			e1.printStackTrace();
-		} catch (UnsupportedEncodingException e1) {
-			e1.printStackTrace();
-		}
-		logger.setUseParentHandlers(false);
-	}
-	
+	private Logger logger = MyUtil.getLogger(CommentService.class.getName());
+
 	public void publishComment(Comment comment) {
 		logger.info("출판한 코멘트: " + comment.getText());
 	}
