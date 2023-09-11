3ea7f025b8594d5de32e70a892e940542a1f423e
osparking <jbpark03@gmail.com>
2023-09-11 23:18:12 +0900

    implement aspect logic of logging before and after

--- src/main/java/aspect/LoggingAspect.java
+++ src/main/java/aspect/LoggingAspect.java
@@ -1,16 +1,23 @@
 package aspect;
 
+import java.util.logging.Logger;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
+import util.MyUtil;
+
 @Aspect
 public class LoggingAspect {
+	private Logger logger = MyUtil.getLogger(LoggingAspect.class.getName());
 	
 	@Around("execution(* services.*.*(..))")
 	public void log(ProceedingJoinPoint joinPoint) {
 		try {
+			logger.info("포인트 컷 메소드 호출 전");
 			joinPoint.proceed();
+			logger.info("포인트 컷 메소드 실행 후");
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
