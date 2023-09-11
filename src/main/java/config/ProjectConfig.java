a7cf0a5352f42ba50cf43513016c2a1ad8fd3eaa
osparking <jbpark03@gmail.com>
2023-09-11 10:29:52 +0900

    specify package to scan for beans

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -1,15 +1,10 @@
 package config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import services.CommentService;
-
 @Configuration
+@ComponentScan(basePackages = {"services"})
 public class ProjectConfig {
-	
-	@Bean
-	CommentService commentService() {
-		return new CommentService();
-	}
+
 }
