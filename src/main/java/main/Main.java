aa4e8b5083f313fb200595d1cde47799ee7bedb7
osparking <jbpark03@gmail.com>
2023-09-11 10:37:25 +0900

    publish a dummy comment using service

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import config.ProjectConfig;
+import domain.Comment;
 import services.CommentService;
 
 public class Main {
@@ -10,6 +11,12 @@ public class Main {
 	public static void main(String[] args) {
 		var ctx = new AnnotationConfigApplicationContext(ProjectConfig.class);
 		var service = ctx.getBean(CommentService.class);
+		
+		var comment = new Comment();
+		comment.setText("코맨트 시험");
+		comment.setAuthor("박작가");
+		
+		service.publishComment(comment);
 		System.out.println(service.getClass());
 	}
 
